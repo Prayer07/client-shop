6ca399f6269e244d951cc3fb93f6c115eec05925
Prayer07 <ezenwaokechukwu3@gmail.com>
2026-06-03 20:31:28 +0100

    kkj

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { lazy, Suspense } from 'react'
 import Header from './components/Header'
 // import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import FloatingImg from './components/FloatingImg'
+// import FloatingImg from './components/FloatingImg'
 
 const Home = lazy(() => import('./pages/Home'))
 const Portfolio = lazy(() => import('./pages/Services'))
